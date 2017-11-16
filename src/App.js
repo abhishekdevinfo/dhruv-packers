d4c91bfa8edb7055ca6d57718ac2ef9854d1bce5
abhishekdevinfo <abhi.info.dev@gmail.com>
2017-11-16 15:09:22 +0530

    Added .eslintrc and formating

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
 import AppBar from "material-ui/AppBar";
 import Drawer from "material-ui/Drawer";
 import MenuItem from "material-ui/MenuItem";
-import dhrTheme from './dhrTheme'
+import dhrTheme from "./dhrTheme";
 import injectTapEventPlugin from "react-tap-event-plugin";
 
 injectTapEventPlugin();
@@ -18,25 +18,24 @@ class App extends Component {
 
   render() {
     return (
-
-    <MuiThemeProvider muiTheme={getMuiTheme(dhrTheme)}>
-      <div className="App">
-        <AppBar
-          title="Dhruv Packers"
-          iconClassNameRight="muidocs-icon-navigation-expand-more"
-          onLeftIconButtonTouchTap={() => this.setState({ open: !this.state.open })}
-        />
-        <Drawer
-          docked={false}
-          open={this.state.open}
-          onRequestChange={open => this.setState({ open })}
-        >
-          <MenuItem onClick={this.handleClose}>Menu Item</MenuItem>
-          <MenuItem onClick={this.handleClose}>Menu Item 2</MenuItem>
-        </Drawer>
-      </div>      
-    </MuiThemeProvider>
-
+      <MuiThemeProvider muiTheme={getMuiTheme(dhrTheme)}>
+        <div className="App">
+          <AppBar
+            title="Dhruv Packers"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+            onLeftIconButtonTouchTap={() =>
+              this.setState({ open: !this.state.open })}
+          />
+          <Drawer
+            docked={false}
+            open={this.state.open}
+            onRequestChange={open => this.setState({ open })}
+          >
+            <MenuItem onClick={this.handleClose}>Menu Item</MenuItem>
+            <MenuItem onClick={this.handleClose}>Menu Item 2</MenuItem>
+          </Drawer>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/dhrTheme.js
+++ src/dhrTheme.js
@@ -3,17 +3,24 @@
  */
 
 import {
-  deepPurple500, cyan500, cyan700,
+  deepPurple500,
+  cyan500,
+  cyan700,
   pinkA200,
-  grey100, grey300, grey400, grey500,
-  white, darkBlack, fullBlack,
-} from 'material-ui/styles/colors';
-import {fade} from 'material-ui/utils/colorManipulator';
-import spacing from 'material-ui/styles/spacing';
+  grey100,
+  grey300,
+  grey400,
+  grey500,
+  white,
+  darkBlack,
+  fullBlack
+} from "material-ui/styles/colors";
+import { fade } from "material-ui/utils/colorManipulator";
+import spacing from "material-ui/styles/spacing";
 
 export default {
   spacing: spacing,
-  fontFamily: 'Roboto, sans-serif',
+  fontFamily: "Roboto, sans-serif",
   palette: {
     primary1Color: deepPurple500,
     primary2Color: cyan700,
@@ -28,6 +35,6 @@ export default {
     disabledColor: fade(darkBlack, 0.3),
     pickerHeaderColor: cyan500,
     clockCircleColor: fade(darkBlack, 0.07),
-    shadowColor: fullBlack,
-  },
+    shadowColor: fullBlack
+  }
 };
