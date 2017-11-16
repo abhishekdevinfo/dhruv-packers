0b20a1dfb1ff247165a2a6f3b47d83670f5a84e3
abhishekdevinfo <abhi.info.dev@gmail.com>
2017-11-16 18:37:45 +0530

    Add & apply react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import getMuiTheme from "material-ui/styles/getMuiTheme";
 import AppBar from "material-ui/AppBar";
@@ -14,28 +15,67 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = { open: false };
+
+    this.handleToggle = this.handleToggle.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleToggle() {
+    this.setState({ open: !this.state.open });
+  }
+
+  handleClose() {
+    this.setState({ open: !this.state.open });
   }
 
   render() {
     return (
-      <MuiThemeProvider muiTheme={getMuiTheme(dhrTheme)}>
-        <div className="App">
-          <AppBar
-            title="Dhruv Packers"
-            iconClassNameRight="muidocs-icon-navigation-expand-more"
-            onLeftIconButtonTouchTap={() =>
-              this.setState({ open: !this.state.open })}
-          />
-          <Drawer
-            docked={false}
-            open={this.state.open}
-            onRequestChange={open => this.setState({ open })}
-          >
-            <MenuItem onClick={this.handleClose}>Menu Item</MenuItem>
-            <MenuItem onClick={this.handleClose}>Menu Item 2</MenuItem>
-          </Drawer>
-        </div>
-      </MuiThemeProvider>
+      <BrowserRouter>
+        <MuiThemeProvider muiTheme={getMuiTheme(dhrTheme)}>
+          <div className="App">
+            <AppBar
+              title="Dhruv Packers"
+              iconClassNameRight="muidocs-icon-navigation-expand-more"
+              onLeftIconButtonTouchTap={this.handleToggle}
+            />
+            <Drawer
+              docked={false}
+              open={this.state.open}
+              onRequestChange={open => this.setState({ open })}
+            >
+              <Link to="/">
+                <MenuItem onClick={this.handleClose}>Home</MenuItem>
+              </Link>
+              <Link to="/products">
+                <MenuItem onClick={this.handleClose}>Products</MenuItem>
+              </Link>
+              <Link to="/services">
+                <MenuItem onClick={this.handleClose}>Services</MenuItem>
+              </Link>
+              <Link to="/aboutUs">
+                <MenuItem onClick={this.handleClose}>About Us</MenuItem>
+              </Link>
+            </Drawer>
+
+            <Route exact path="/" render={() => <h1>Home View</h1>} />
+            <Route
+              exact
+              path="/products"
+              render={() => <h1>Products View</h1>}
+            />
+            <Route
+              exact
+              path="/services"
+              render={() => <h1>Services View</h1>}
+            />
+            <Route
+              exact
+              path="/aboutUs"
+              render={() => <h1>About Us View</h1>}
+            />
+          </div>
+        </MuiThemeProvider>
+      </BrowserRouter>
     );
   }
 }
