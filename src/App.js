f6ed3f0a0d605e5900f4d869b1f6ffc743d1b169
abhishekdevinfo <abhi.info.dev@gmail.com>
2017-11-16 14:49:46 +0530

    Add: 1. Drawer 2. Custom theme & Arrange everything

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-import React, { Component } from 'react';
-import {RaisedButton} from 'material-ui';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from "material-ui/AppBar";
+import Drawer from "material-ui/Drawer";
+import MenuItem from "material-ui/MenuItem";
+import dhrTheme from './dhrTheme'
+import injectTapEventPlugin from "react-tap-event-plugin";
+
+injectTapEventPlugin();
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
+  }
+
   render() {
     return (
+
+    <MuiThemeProvider muiTheme={getMuiTheme(dhrTheme)}>
       <div className="App">
-         
-      </div>
+        <AppBar
+          title="Dhruv Packers"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+          onLeftIconButtonTouchTap={() =>
+            this.setState({ open: !this.state.open })}
+        />
+        <Drawer
+          docked={false}
+          open={this.state.open}
+          onRequestChange={open => this.setState({ open })}
+        >
+          <MenuItem onClick={this.handleClose}>Menu Item</MenuItem>
+          <MenuItem onClick={this.handleClose}>Menu Item 2</MenuItem>
+        </Drawer>
+      </div>      
+    </MuiThemeProvider>
+
     );
   }
 }
